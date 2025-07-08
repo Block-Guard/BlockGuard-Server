ce8f39652b8d062d51aac2bd80015b789944f34e
Chae-Yu <chaeyu2004@naver.com>
2025-07-08 22:57:47 +0900

    refactor: allocate more time

--- src/main/java/com/blockguard/server/domain/auth/enums/JwtTokenExpireTime.java
+++ src/main/java/com/blockguard/server/domain/auth/enums/JwtTokenExpireTime.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum JwtTokenExpireTime {
     ACCESS_TOKEN_EXPIRE_TIME(86400000),
-    REFRESH_TOKEN_EXPIRE_TIME(86400000);
+    REFRESH_TOKEN_EXPIRE_TIME(100000000);
 
     private final long expireTime;
 

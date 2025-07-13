5dd5127ca5b3738d42b64d7537d08326f6dceacd
yeonjy73 <yeon703@naver.com>
2025-07-13 16:39:08 +0900

    feat: FraudPhoneNumber Entity 작성

---
+++ src/main/java/com/blockguard/server/domain/fraud/phonenumber/domain/FraudPhoneNumber.java
@@ -0,0 +1,35 @@
+package com.blockguard.server.domain.fraud.phonenumber.domain;
+
+import com.blockguard.server.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "fraud_phone_numbers")
+public class FraudPhoneNumber extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(name = "phone_number", nullable = false, length = 20, unique = true)
+    private String phoneNumber;
+
+    // Todo: Open API 제공자 추후 enum type 변경
+    @Column(nullable = false, length = 100)
+    private String provider;
+
+    @Column(name = "last_checked_at", nullable = false)
+    private LocalDateTime lastCheckedAt;
+
+    public void updateLastCheckedAt(LocalDateTime checkedAt) {
+        this.lastCheckedAt = checkedAt;
+    }
+}

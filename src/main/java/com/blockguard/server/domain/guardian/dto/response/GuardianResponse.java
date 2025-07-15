673d27854ac26e9c151a64c2ab521e693560e490
yeonjy73 <yeon703@naver.com>
2025-07-16 02:15:03 +0900

    fix: response body 수정 primary 프로퍼티 무시 isPrimary 형식으로 수정

--- src/main/java/com/blockguard/server/domain/guardian/dto/response/GuardianResponse.java
+++ src/main/java/com/blockguard/server/domain/guardian/dto/response/GuardianResponse.java
@@ -1,5 +1,7 @@
 package com.blockguard.server.domain.guardian.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,12 +10,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties("primary") // 자동으로 생성되는 "primary" 프로퍼티 무시
 public class GuardianResponse {
     private Long guardiansId;
     private String name;
 
     @Schema(description = "전화번호 (010-1234-5678 형식)", example = "010-1234-5678")
     private String phoneNumber;
+
+    @JsonProperty("isPrimary")
     private boolean isPrimary;
     private String profileImageUrl;
     private String createdAt;

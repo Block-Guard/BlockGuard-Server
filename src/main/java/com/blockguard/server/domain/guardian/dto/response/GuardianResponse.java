e5fe796ade68bba1986768d322108d231fbfaf0f
yeonjy73 <yeon703@naver.com>
2025-07-16 02:02:59 +0900

    fix: add Schema phoneNumber response dto

--- src/main/java/com/blockguard/server/domain/guardian/dto/response/GuardianResponse.java
+++ src/main/java/com/blockguard/server/domain/guardian/dto/response/GuardianResponse.java
@@ -1,5 +1,6 @@
 package com.blockguard.server.domain.guardian.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,8 @@ import lombok.Getter;
 public class GuardianResponse {
     private Long guardiansId;
     private String name;
+
+    @Schema(description = "전화번호 (010-1234-5678 형식)", example = "010-1234-5678")
     private String phoneNumber;
     private boolean isPrimary;
     private String profileImageUrl;

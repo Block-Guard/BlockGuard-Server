d6dfc3bf69fc02e93e5d574f47441129e05c3a62
yeonjy73 <yeon703@naver.com>
2025-07-16 17:47:54 +0900

    fix: add s3 getGuardiansList

--- src/main/java/com/blockguard/server/domain/user/application/GuardianService.java
+++ src/main/java/com/blockguard/server/domain/user/application/GuardianService.java
@@ -4,6 +4,7 @@ import com.blockguard.server.domain.guardian.dao.GuardianRepository;
 import com.blockguard.server.domain.guardian.dto.response.GuardianResponse;
 import com.blockguard.server.domain.guardian.dto.response.GuardiansListResponse;
 import com.blockguard.server.domain.user.domain.User;
+import com.blockguard.server.global.config.S3.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,22 +13,28 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class GuardianService {
     private final GuardianRepository guardianRepository;
+    private final S3Service s3Service;
 
     @Transactional
     public GuardiansListResponse getGuardiansList(User user) {
         return GuardiansListResponse.builder()
                 .guardians(
                         guardianRepository.findAllByUserId(user.getId()).stream()
-                                .map(guardian -> GuardianResponse.builder()
+                                .map(guardian ->
+                                        GuardianResponse.builder()
                                         .guardiansId(guardian.getId())
                                         .name(guardian.getName())
                                         .phoneNumber(guardian.getPhoneNumber())
                                         .isPrimary(guardian.isPrimary())
                                         .createdAt(guardian.getCreatedAt().toString())
-                                        .profileImageUrl(guardian.getProfileImageKey()) //Todo: s3 로직
+                                        .profileImageUrl(
+                                                guardian.getProfileImageKey() != null
+                                                        ? s3Service.getPublicUrl(guardian.getProfileImageKey())
+                                                        : null
+                                        )
                                         .build()
                                 ).toList()
-                        )
+                )
                 .build();
 
     }

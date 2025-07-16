dc8b6e8243973afc12f1ad3f9824377d767eebe1
yeonjy73 <yeon703@naver.com>
2025-07-17 03:01:40 +0900

    feat: create guardian api

--- src/main/java/com/blockguard/server/domain/guardian/api/GuardianApi.java
+++ src/main/java/com/blockguard/server/domain/guardian/api/GuardianApi.java
@@ -1,7 +1,9 @@
 package com.blockguard.server.domain.guardian.api;
 
+import com.blockguard.server.domain.guardian.dto.request.CreateGuardianRequest;
+import com.blockguard.server.domain.guardian.dto.response.GuardianResponse;
 import com.blockguard.server.domain.guardian.dto.response.GuardiansListResponse;
-import com.blockguard.server.domain.user.application.GuardianService;
+import com.blockguard.server.domain.guardian.application.GuardianService;
 import com.blockguard.server.domain.user.domain.User;
 import com.blockguard.server.global.common.codes.SuccessCode;
 import com.blockguard.server.global.common.response.BaseResponse;
@@ -10,7 +12,9 @@ import com.blockguard.server.global.config.swagger.CustomExceptionDescription;
 import com.blockguard.server.global.config.swagger.SwaggerResponseDescription;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +30,16 @@ public class GuardianApi {
     public ResponseEntity<BaseResponse<GuardiansListResponse>> getGuardiansList(@Parameter(hidden = true) @CurrentUser User user) {
         GuardiansListResponse guardiansList = guardianService.getGuardiansList(user);
         return ResponseEntity.ok(BaseResponse.of(SuccessCode.GUARDIANS_FOUND, guardiansList));
-
     }
+
+    @Operation(summary = "보호자 등록")
+    @CustomExceptionDescription(SwaggerResponseDescription.FIND_GUARDIANS_FAIL)
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<BaseResponse<GuardianResponse>> createGuardian(@Parameter(hidden = true) @CurrentUser User user,
+                                                                         @Valid CreateGuardianRequest createGuardianRequest) {
+        GuardianResponse guardianResponse = guardianService.createGuardian(user, createGuardianRequest);
+        return ResponseEntity.ok(BaseResponse.of(SuccessCode.CREATE_GUARDIAN_SUCCESS, guardianResponse));
+    }
+
+
 }

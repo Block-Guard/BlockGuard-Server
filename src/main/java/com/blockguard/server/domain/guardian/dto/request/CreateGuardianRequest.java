e034e3f54173201dd6cca7717191dbb52bda5c26
yeonjy73 <yeon703@naver.com>
2025-07-17 03:18:21 +0900

    chore: swagger 에서 send empty value 클릭시 오류 발생

--- src/main/java/com/blockguard/server/domain/guardian/dto/request/CreateGuardianRequest.java
+++ src/main/java/com/blockguard/server/domain/guardian/dto/request/CreateGuardianRequest.java
@@ -1,10 +1,12 @@
 package com.blockguard.server.domain.guardian.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.lang.Nullable;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
@@ -18,6 +20,6 @@ public class CreateGuardianRequest {
     @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "010-1234-5678 형식이어야 합니다.")
     private String phoneNumber;
 
-    // 선택 필드
+    @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private MultipartFile profileImage;
 }

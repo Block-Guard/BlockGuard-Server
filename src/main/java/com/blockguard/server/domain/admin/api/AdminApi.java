47b5eb41c84bd729c1baf8d85a5e9e89949a491c
Chae-Yu <chaeyu2004@naver.com>
2025-07-29 11:59:02 +0900

    feat: call open data api

---
+++ src/main/java/com/blockguard/server/domain/admin/api/AdminApi.java
@@ -0,0 +1,25 @@
+package com.blockguard.server.domain.admin.api;
+
+import com.blockguard.server.global.common.codes.SuccessCode;
+import com.blockguard.server.global.common.response.BaseResponse;
+import com.blockguard.server.infra.scheduler.FraudUrlImporter;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/admin")
+public class AdminApi {
+
+    private final FraudUrlImporter fraudUrlImporter;
+
+    @GetMapping("/update/fraud-url")
+    @Operation(summary = "공공 api 데이터 호출 - 관리자용")
+    public BaseResponse<Void> syncFraudUrls(){
+        fraudUrlImporter.syncFraudUrlsFromOpenApi();
+        return BaseResponse.of(SuccessCode.IMPORT_OPEN_API_SUCCESS);
+    }
+}

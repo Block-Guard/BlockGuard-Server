89d51f5552564f748d104a6f40a9c50b83b233b5
Chae-Yu <chaeyu2004@naver.com>
2025-08-07 15:20:39 +0900

    feat: permit get selected news api

--- src/main/java/com/blockguard/server/global/config/SecurityConfig.java
+++ src/main/java/com/blockguard/server/global/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
                                 .requestMatchers("/api/auth/**").permitAll()   // 로그인 및 회원가입
                                 .requestMatchers("/api/admin/login").permitAll()   // 관리자 로그인
                                 .requestMatchers("/api/fraud/url", "/api/fraud/number","/api/fraud-analysis").permitAll() // 사기 분석
-                                .requestMatchers("/api/news").permitAll() // 뉴스 조회
+                                .requestMatchers("/api/news","/api/news/selected").permitAll() // 뉴스 조회
                                 .requestMatchers("/actuator/health").permitAll() // 헬스체크 허용
                                 .requestMatchers("/", "/index.html", "/favicon.ico").permitAll()
                                 .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/webjars/**").permitAll()

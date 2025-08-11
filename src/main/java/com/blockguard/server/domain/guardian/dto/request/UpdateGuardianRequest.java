33418180f2b224cd2c97fcd804670a9d925574d2
yeonjy73 <yeon703@naver.com>
2025-08-11 23:30:38 +0900

    fix: size 제약 조건 추가

--- src/main/java/com/blockguard/server/domain/guardian/dto/request/UpdateGuardianRequest.java
+++ src/main/java/com/blockguard/server/domain/guardian/dto/request/UpdateGuardianRequest.java
@@ -3,9 +3,11 @@ package com.blockguard.server.domain.guardian.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
@@ -13,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Builder
 public class UpdateGuardianRequest {
     @NotBlank
+    @Size(max = 50, message = "name length must be <= 50")
     private String name;
 
     @NotBlank

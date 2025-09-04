25c110c4c54a64566ce75e949b4258fbc604e265
Chae-Yu <chaeyu2004@naver.com>
2025-09-04 15:53:13 +0900

    refactor: add @NoArgsConstructor annotation to deserialize

--- src/main/java/com/blockguard/server/domain/news/dto/response/NewsPageResponse.java
+++ src/main/java/com/blockguard/server/domain/news/dto/response/NewsPageResponse.java
@@ -3,12 +3,14 @@ package com.blockguard.server.domain.news.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class NewsPageResponse {
     private List<NewsArticleResponse> news;
     private PageableInfo pageableInfo;
